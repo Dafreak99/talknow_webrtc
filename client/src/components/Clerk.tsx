c162f04e852c2a67080527c2f5f713813d813b58
tranchauhai99 <hai.tran@ptnglobalcorp.com>
2021-11-06 00:31:06 +0700

    change afterSignInUrl

--- client/src/components/Clerk.tsx
+++ client/src/components/Clerk.tsx
@@ -6,31 +6,35 @@ import { useHistory } from "react-router";
 interface Props {}
 
 const Clerk: React.FC<Props> = () => {
-  const { push } = useHistory();
+	const { push } = useHistory();
 
-  return (
-    <Flex
-      justify="center"
-      alignItems="center"
-      h="100vh"
-      w="100vw"
-      background="primary"
-    >
-      <Flex
-        position="absolute"
-        top="5%"
-        left="5%"
-        color="#c7c7cb"
-        alignItems="center"
-        cursor="pointer"
-        onClick={() => push("/")}
-      >
-        <Box as={MdKeyboardBackspace} mr="0.5rem" boxSize="1.5rem" />
-        <Text fontSize="1.5rem">Back</Text>
-      </Flex>
-      <SignIn routing="path" path="/sign-in" />
-    </Flex>
-  );
+	return (
+		<Flex
+			justify="center"
+			alignItems="center"
+			h="100vh"
+			w="100vw"
+			background="primary"
+		>
+			<Flex
+				position="absolute"
+				top="5%"
+				left="5%"
+				color="#c7c7cb"
+				alignItems="center"
+				cursor="pointer"
+				onClick={() => push("/")}
+			>
+				<Box as={MdKeyboardBackspace} mr="0.5rem" boxSize="1.5rem" />
+				<Text fontSize="1.5rem">Back</Text>
+			</Flex>
+			<SignIn
+				routing="path"
+				path="/sign-in"
+				afterSignInUrl="http://159.223.77.114:3000"
+			/>
+		</Flex>
+	);
 };
 
 export default Clerk;
